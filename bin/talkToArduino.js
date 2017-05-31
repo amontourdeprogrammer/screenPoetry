57cdf1b2f6bc16230c0b0658f11316a001b6383f
Cécile Briard <cecile@artips.fr>
2017-05-31 15:49:58 +0200

    Send user input to Arduino once

---
+++ bin/talkToArduino.js
@@ -0,0 +1,23 @@
+var SerialPort = require('serialport');
+require('dotenv').config();
+
+//serial listener
+function serialListener(userInput)
+{
+  var receivedData = "";
+  serialPort = new SerialPort(process.env.ARDUINOPORTNAME, {
+      baudrate: 9600,
+      // defaults for Arduino serial communication
+       dataBits: 8,
+       parity: 'none',
+       stopBits: 1,
+       flowControl: false
+  });
+
+  serialPort.on("open", function () {
+        console.log('open serial communication');
+      serialPort.write(userInput);
+  });
+}
+
+module.exports = {serialListener};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const {serialListener} = require('../bin/talkToArduino.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // write something to serial port
@@ -9,7 +9,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function (req, res) {
-  res.render('index', { title: 'Screen Poetry', userInput: req.body.userInput });
+	serialListener(req.body.userInput);
+	res.render('index', { title: 'Screen Poetry', userInput: req.body.userInput });
 });
 
 module.exports = router;
+
+var SerialPort = require('serialport');
+require('dotenv').config();
